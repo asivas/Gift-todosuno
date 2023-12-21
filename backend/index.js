2820a1a1894356ce60f334a0e930910835ce42ce
sofia perone <sofiaperone62@gmail.com>
2023-12-21 09:19:51 -0300

    jade

--- backend/index.js
+++ backend/index.js
@@ -25,8 +25,8 @@ app.use(
 
 app.use(bodyParser.json({ limit: "5mb" }));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+//app.set("views", path.join(__dirname, "views"));
+//app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());

c56e9d9c5f5714ea7adff8a35650b8f8ef41e272
sofia perone <sofiaperone62@gmail.com>
2023-12-23 10:34:42 -0300

    front2

--- frontend/src/components/quienesSomos/quienesSomos.jsx
+++ frontend/src/components/quienesSomos/quienesSomos.jsx
@@ -6,8 +6,8 @@ const QuienesSomos = () => {
     <div className='quienesSomos'>
     <div className='texto'>
     <h1>¿Quienes</h1><h1 className='sub'>Somos?</h1>
-      <p>Somos una vibrante comunidad nacida en la encantadora ciudad de Tandil, comprometida con el bienestar colectivo. Nos destacamos por crear y utilizar el mejor sistema disponible de ayuda mutua, diseñado para brindar beneficios accesibles a todas las personas a nivel global. Nos definimos como una comunidad colaborativa, donde la solidaridad y el espíritu de ayuda mutua son los pilares fundamentales.
-      ¿Nuestro enfoque mas fuerte? la ley universal,<span> dar para recibir.</span></p>
+      <p>Somos una comunidad 100% enfocada en el crecimiento colectivo, mediante el desarrollo de sus participantes, creando un sistema de ayuda mutua, donde el complemento de las habilidades de personas de todo el mundo 🌍 se alinean para generar beneficios mayores a los que puede una persona por sí misma.
+Con la firme creencia que <span> cooperar da mejores resultados que competir.</span></p>
     </div>
     </div>
   )

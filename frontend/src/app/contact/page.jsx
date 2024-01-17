755273312b1dd332a8bae2a71ce03a00ea3da075
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2024-01-17 17:31:45 -0300

    Removed spain phone

--- frontend/src/app/contact/page.jsx
+++ frontend/src/app/contact/page.jsx
@@ -16,8 +16,7 @@ const contact = () => {
     <h2>CONTACTANOS</h2>
     <p>Hola! Si estas interesado en nuestra comunidad y te gustaria aprender mas , o sumarte al equipo, no dudes en contactarte con nuestros representantes! Podes encontrarlos en los siguientes telefonos y mail.</p> 
     <li>soportetodosuno@gmail.com</li>
-    <li>+5491134228779</li>
-    <li>+34627278370</li>
+    <li>+54 9 11 3422 8779</li>
     </div>
     </div>
     </div>

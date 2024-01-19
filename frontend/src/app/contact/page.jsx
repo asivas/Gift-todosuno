249978526a2781cd46f097401057060405ff27d3
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2024-01-19 10:19:06 -0300

    .env.prod template added, reformatted phone on contact

--- frontend/src/app/contact/page.jsx
+++ frontend/src/app/contact/page.jsx
@@ -5,22 +5,26 @@ import Link from 'next/link'
 import "./contact.css"
 
 const contact = () => {
-  
-  return (
-    <div className='contact'>
-    <Banner/>
-    <div className='back'><Link href={"/"}>Volver al inicio</Link></div>
-    
-    <div className='contactCard'> 
-    <div className='cc'>
-    <h2>CONTACTANOS</h2>
-    <p>Hola! Si estas interesado en nuestra comunidad y te gustaria aprender mas , o sumarte al equipo, no dudes en contactarte con nuestros representantes! Podes encontrarlos en los siguientes telefonos y mail.</p> 
-    <li>soportetodosuno@gmail.com</li>
-    <li>+54 9 11 3422 8779</li>
-    </div>
-    </div>
-    </div>
-  )
+
+    return (
+        <div className='contact'>
+            <Banner/>
+            <div className='back'><Link href={"/"}>Volver al inicio</Link></div>
+
+            <div className='contactCard'>
+                <div className='cc'>
+                    <h2>CONTACTANOS</h2>
+                    <p>Hola! Si estas interesado en nuestra comunidad y te gustaria aprender mas , o sumarte al equipo,
+                        no dudes en contactarte con nuestros representantes! Podes encontrarlos en los siguientes
+                        telefonos y mail.</p>
+                    <ul>
+                        <li>soportetodosuno@gmail.com</li>
+                        <li>+5491134228779</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default contact

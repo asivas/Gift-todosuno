d34689dbffc6f3f5466ad6acb1e17cc1ebc96d9a
emamanriquez <emamanriquez1991@gmail.com>
2024-04-08 20:17:19 -0300

    Fix Actualizacion 08/04

--- frontend/src/components/banner/banner.jsx
+++ frontend/src/components/banner/banner.jsx
@@ -37,7 +37,7 @@ const Banner = () => {
           });
 
           document.getElementById("modo-libre").addEventListener("click", function() {
-            window.location.href = "/test";
+            window.location.href = "/registerFree";
           });
           `,
         }}
